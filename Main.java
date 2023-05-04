ff42477b5feba3a9387f17d8f06005cccda91803
mariancd <marian@innertrends.com>
2023-05-04 14:13:52 +0300

    Basic operations.

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
-    public class Main {
-        public static void main(String[] args) {
-            System.out.println("Hello World");
+    /**
+     * Created: 04.05.2023
+     * Author: Marian M.
+     * Description: Learning Java step by step
+     * 
+     */
+    public class Main { // All the code must be placed inside a class, having the same name as the file
+        public static void main(String[] args) { // The "main" method is mandatory
+            /*System.out.println("Hello World");
+            String txt = "We are the so-called 'Vikings' from the north.";
+            txt = "It's alright.";
+            System.out.println(txt);
+            aSimpleMethod();
+            */
+            int x = 10;
+            for (int i = 0; i < x; i++){
+                System.out.println(i);
+            }
+            //System.out.println(i);
+        }
+
+        static void aSimpleMethod(){
+            System.out.println("aici");
         }
     }
